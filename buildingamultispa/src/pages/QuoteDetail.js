a3219b3b15ab5b80ccf0e331c911e24db021f995
omar saade <omarsaadeh11@gmail.com>
2022-08-31 23:07:51 +0300

    react

--- buildingamultispa/src/pages/QuoteDetail.js
+++ buildingamultispa/src/pages/QuoteDetail.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link, Route, useParams, useRouteMatch, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, Route, useParams, useRouteMatch } from 'react-router-dom'
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-    { id: 'q2', author: 'Maximlian', text: 'Learning React is great!' },
-]
+// const DUMMY_QUOTES = [
+//     { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+//     { id: 'q2', author: 'Maximlian', text: 'Learning React is great!' },
+// ]
 
 
 
@@ -17,18 +20,40 @@ function QuoteDetail() {
     const match = useRouteMatch();
     const params = useParams();
 
+    const { quoteId } = params;
+
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true)
     // console.log(params);// { quoteId: 'q1' }
     // console.log(match); // { path: '/quotes/:quoteId', url: '/quotes/q2', isExact: true, params: {… } }
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    // const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
 
-    if (!quote) {
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+
+    if (status === 'pending') {
+        return <div className='centered'>
+            <LoadingSpinner />
+        </div>
+    }
+
+
+    if (error) {
+        return <p className='centered focused'>{error}</p>
+    }
+
+
+
+    if (!loadedQuote.text) {
         return <p>no quote found!</p>;
     }
 
     return (
         <h1>
 
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
             {/* <Route path={`/quotes/${params.quoteId}`} exact> */}
 
             <Route path={match.path} exact>
